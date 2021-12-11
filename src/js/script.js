4f47b63fc183fc6e00c5426a9aef0aac8eb1fd1d
Krzysztof Podgórski <k4podgorski@gmail.com>
2021-12-11 11:51:33 +0100

    Add price calculation script (processOrderr, initOrderForm)

--- src/js/script.js
+++ src/js/script.js
@@ -60,9 +60,11 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
-      console.log('new product:', thisProduct);
     }
 
     renderInMenu(){
@@ -76,11 +78,20 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion(){
       const thisProduct = this;
-      const clickableTrigger = this.element.querySelector(select.menuProduct.clickable);
   
-      clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
         event.preventDefault();
 
         const activeProduct = document.querySelector(select.all.menuProductsActive);
@@ -91,6 +102,55 @@
 
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
+    }
+
+    initOrderForm(){
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder() {
+      const thisProduct = this;
+    
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      let price = thisProduct.data.price;
+    
+      for(let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+    
+        for(let optionId in param.options) {
+          const option = param.options[optionId];
+
+          if(formData[paramId] && formData[paramId].includes(optionId)){
+            
+            if(!option.default === true){
+              price += option.price
+            }
+          }else{
+
+            if(option.default === true){
+              price -= option.price
+            }
+          }
+        } 
+      }
+    
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
