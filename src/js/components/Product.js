be7cffed5ce71ff11f7cc8ac5ded396e57e766ed
unknown <k4podgorski@gmail.com>
2022-02-18 22:00:38 +0100

    fix import

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -1,6 +1,6 @@
-import {classNames, select, templates} from './../settings.js';
+import {classNames, select, templates} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
-import utils from './../utils.js';
+import utils from '../utils.js';
 
 class Product{
   constructor(id, data){
