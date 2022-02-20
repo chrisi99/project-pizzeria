ba7e1935dbe8a3746b40bf7150ea78ae772517f9
unknown <k4podgorski@gmail.com>
2022-02-20 18:19:12 +0100

    Download data from API

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,7 +1,8 @@
-import {templates, select } from '../settings.js';
+import {templates, select, settings } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
+import utils from '../utils.js';
 
 class Booking{
   constructor(element){
@@ -9,6 +10,58 @@ class Booking{
 
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
+  }
+
+  getData(){
+    const thisBooking = this;
+
+    const startDayParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePickerWidget.minDate);
+    const endDayParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePickerWidget.maxDate);
+    
+    const params = {
+      booking: [
+        startDayParam,
+        endDayParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDayParam,
+        endDayParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDayParam,
+      ],
+    };
+    //console.log('getData', params);
+
+    const urls = {
+      booking: settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat: settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat.join('&'),
+    };
+    //console.log('getdataurl', urls );
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrent = allResponses[1];
+        const eventsRepeat = allResponses[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrent.json(),
+          eventsRepeat.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
   }
 
   render(element){
